944dd0dc0c8521a46a44a52118adf954f3a411c5
Vaishnavi <vj222@njit.edu>
2018-11-01 16:08:08 -0400

    Unit Test: Inserting Car details

---
+++ tests/Unit/CarInsertTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class CarInsertTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = factory(\App\car::class)->create();
+        $this->assertDatabaseHas('cars', ['make' =>$user->make]);
+    }
+}
