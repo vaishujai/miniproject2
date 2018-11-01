89900d26b6939d4d73baf92ff60f116584320efe
Vaishnavi <vj222@njit.edu>
2018-11-01 17:27:58 -0400

    Unit Test: Make test

---
+++ tests/Unit/CarMakeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\car;
+
+class CarMakeTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $make=car::find(25);
+        $find=$make->make;
+        $this->assertContains($find,['ford','honda','toyota']);
+    }
+}
